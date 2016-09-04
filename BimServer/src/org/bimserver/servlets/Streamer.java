aedc02758ec052ad5a055440eeb3c77e85c695da
Brian-Walker <mjjmusic@yeah.net>
2016-09-04 19:00:19 +0800

    创建
    来自bimserver-master 2016/9/14

---
+++ BimServer/src/org/bimserver/servlets/Streamer.java
@@ -0,0 +1,188 @@
+package org.bimserver.servlets;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.GregorianCalendar;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.apache.commons.io.output.NullWriter;
+import org.bimserver.BimServer;
+import org.bimserver.endpoints.EndPoint;
+import org.bimserver.longaction.LongAction;
+import org.bimserver.longaction.LongDownloadOrCheckoutAction;
+import org.bimserver.longaction.LongStreamingDownloadAction;
+import org.bimserver.models.log.AccessMethod;
+import org.bimserver.plugins.serializers.SerializerException;
+import org.bimserver.plugins.serializers.Writer;
+import org.bimserver.shared.StreamingSocketInterface;
+import org.bimserver.shared.exceptions.ServerException;
+import org.bimserver.shared.exceptions.UserException;
+import org.bimserver.shared.interfaces.NotificationInterface;
+import org.bimserver.shared.interfaces.RemoteServiceInterface;
+import org.bimserver.utils.Formatters;
+import org.bimserver.webservices.ServiceMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.LittleEndianDataOutputStream;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.stream.JsonReader;
+
+public class Streamer implements EndPoint {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Streamer.class);
+	private long uoid;
+	private long endpointid;
+	private BimServer bimServer;
+	private NotificationInterface notificationInterface;
+	private RemoteServiceInterface remoteServiceInterface;
+	private StreamingSocketInterface streamingSocketInterface;
+
+	public Streamer(StreamingSocketInterface streamingSocketInterface, BimServer bimServer) {
+		this.streamingSocketInterface = streamingSocketInterface;
+		this.bimServer = bimServer;
+		notificationInterface = bimServer.getReflectorFactory().createReflector(NotificationInterface.class, new JsonWebsocketReflector(bimServer.getServicesMap(), streamingSocketInterface));
+		remoteServiceInterface = bimServer.getReflectorFactory().createReflector(RemoteServiceInterface.class, new JsonWebsocketReflector(bimServer.getServicesMap(), streamingSocketInterface));
+	}
+
+	public void onOpen() {
+		JsonObject welcome = new JsonObject();
+		welcome.add("welcome", new JsonPrimitive(new GregorianCalendar().getTimeInMillis()));
+		streamingSocketInterface.send(welcome);
+	}
+	
+	public void onText(Reader reader) {
+		JsonReader jsonreader = new JsonReader(reader);
+		JsonParser parser = new JsonParser();
+		JsonObject request = (JsonObject) parser.parse(jsonreader);
+		if (request.has("hb")) {
+			// Heartbeat, ignore
+		} else if (request.has("action")) {
+			if (request.get("action").getAsString().equals("download")) {
+				final long topicId = request.get("topicId").getAsLong();
+				Thread thread = new Thread(){
+					@Override
+					public void run() {
+						try {
+							LongAction<?> longAction = bimServer.getLongActionManager().getLongAction(topicId);
+							Writer writer = null;
+							if (longAction instanceof LongStreamingDownloadAction) {
+								LongStreamingDownloadAction longStreamingDownloadAction = (LongStreamingDownloadAction)longAction;
+								writer = longStreamingDownloadAction.getMessagingStreamingSerializer();
+							} else {
+								LongDownloadOrCheckoutAction longDownloadAction = (LongDownloadOrCheckoutAction) longAction;
+								writer = longDownloadAction.getMessagingSerializer();
+							}
+							boolean writeMessage = true;
+							int counter = 0;
+							long bytes = 0;
+							long start = System.nanoTime();
+							long totalQT = 0;
+							
+							// We use async websockets, but don't want to fill the websocket buffer with gigabytes of data that's not processed yet on the client
+							// We also don't want to waste any time waiting for the messages to be delivered
+							// This future acts as a 1-message buffer, so that we can fill the next buffer while sending the websocket message
+							Future<Void> future = null;
+
+							// TODO reuse buffer							
+							
+							do {
+								ReusableByteArrayOutputStream byteArrayOutputStream = new ReusableByteArrayOutputStream();
+								LittleEndianDataOutputStream dataOutputStream = new LittleEndianDataOutputStream(byteArrayOutputStream);
+								dataOutputStream.writeLong(topicId);
+//								long s = System.nanoTime();
+								writeMessage = writer.writeMessage(dataOutputStream, null);
+//								int messages = 1;
+//								while (byteArrayOutputStream.size() < BUFFER_SIZE && writeMessage) {
+//									messages++;
+//									writeMessage = writer.writeMessage(byteArrayOutputStream, null);
+//								}
+//								long e = System.nanoTime();
+//								totalQT += (e - s);
+//								dataOutputStream.flush();
+//								byte[] byteArray = byteArrayOutputStream.toByteArray();
+//								ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
+//								byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+//								byteBuffer.putInt(4, messages);
+								
+								if (future != null) {
+									future.get();
+								}
+								
+								if (byteArrayOutputStream.usedSize() > 4) {
+									bytes += byteArrayOutputStream.usedSize();
+									future = streamingSocketInterface.send(byteArrayOutputStream.getByteArray(), 0, byteArrayOutputStream.usedSize());
+									counter++;
+								}
+							} while (writeMessage);
+							long end = System.nanoTime();
+							LOGGER.info("total qt: " + (totalQT / 1000000) + " ms");
+							LOGGER.info(counter + " messages written " + Formatters.bytesToString(bytes) + " in " + ((end - start) / 1000000) + " ms");
+						} catch (IOException e) {
+							// Probably closed/F5-ed browser
+						} catch (SerializerException | InterruptedException | ExecutionException e) {
+							LOGGER.error("", e);
+						}
+					}
+				};
+				thread.setName("Streamer " + topicId);
+				thread.start();
+			}
+		} else if (request.has("token")) {
+			String token = request.get("token").getAsString();
+			try {
+				ServiceMap serviceMap = bimServer.getServiceFactory().get(token, AccessMethod.JSON);
+				uoid = serviceMap.getBimServerAuthInterface().getLoggedInUser().getOid();
+
+				this.endpointid = bimServer.getEndPointManager().register(this);
+				
+				JsonObject enpointMessage = new JsonObject();
+				enpointMessage.add("endpointid", new JsonPrimitive(endpointid));
+				streamingSocketInterface.send(enpointMessage);
+			} catch (UserException e) {
+				LOGGER.error("", e);
+			} catch (ServerException e) {
+				LOGGER.error("", e);
+			}
+		} else {
+			bimServer.getJsonHandler().execute(request, null, new NullWriter());
+		}
+	}
+
+	@Override
+	public void cleanup() {
+		bimServer.getEndPointManager().unregister(endpointid);
+	}
+
+	public void onClose() {
+		LOGGER.debug("onClose, unregistering endpoint " + this.getEndPointId());
+		bimServer.getEndPointManager().unregister(this);
+	}
+
+	@Override
+	public long getEndPointId() {
+		return endpointid;
+	}
+
+	@Override
+	public NotificationInterface getNotificationInterface() {
+		return notificationInterface;
+	}
+
+	@Override
+	public RemoteServiceInterface getRemoteServiceInterface() {
+		return remoteServiceInterface;
+	}
+
+	@Override
+	public long getUoid() {
+		return uoid;
+	}
+	
+	@Override
+	public String toString() {
+		return "Streamer with endpoint " + endpointid;
+	}
+}
---
+++ PluginBase/src/org/bimserver/utils/IfcUtils.java
@@ -0,0 +1,355 @@
+package org.bimserver.utils;
+
+/******************************************************************************
+ * Copyright (C) 2009-2015  BIMserver.org
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
+ *****************************************************************************/
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.bimserver.emf.IdEObject;
+import org.bimserver.models.ifc2x3tc1.IfcAreaMeasure;
+import org.bimserver.models.ifc2x3tc1.IfcAxis2Placement;
+import org.bimserver.models.ifc2x3tc1.IfcAxis2Placement2D;
+import org.bimserver.models.ifc2x3tc1.IfcAxis2Placement3D;
+import org.bimserver.models.ifc2x3tc1.IfcBoolean;
+import org.bimserver.models.ifc2x3tc1.IfcBuildingStorey;
+import org.bimserver.models.ifc2x3tc1.IfcCartesianPoint;
+import org.bimserver.models.ifc2x3tc1.IfcElectricCurrentMeasure;
+import org.bimserver.models.ifc2x3tc1.IfcElement;
+import org.bimserver.models.ifc2x3tc1.IfcElementQuantity;
+import org.bimserver.models.ifc2x3tc1.IfcGridPlacement;
+import org.bimserver.models.ifc2x3tc1.IfcIdentifier;
+import org.bimserver.models.ifc2x3tc1.IfcInteger;
+import org.bimserver.models.ifc2x3tc1.IfcLabel;
+import org.bimserver.models.ifc2x3tc1.IfcLengthMeasure;
+import org.bimserver.models.ifc2x3tc1.IfcLocalPlacement;
+import org.bimserver.models.ifc2x3tc1.IfcLogical;
+import org.bimserver.models.ifc2x3tc1.IfcObject;
+import org.bimserver.models.ifc2x3tc1.IfcObjectDefinition;
+import org.bimserver.models.ifc2x3tc1.IfcObjectPlacement;
+import org.bimserver.models.ifc2x3tc1.IfcPhysicalQuantity;
+import org.bimserver.models.ifc2x3tc1.IfcPlaneAngleMeasure;
+import org.bimserver.models.ifc2x3tc1.IfcPowerMeasure;
+import org.bimserver.models.ifc2x3tc1.IfcProduct;
+import org.bimserver.models.ifc2x3tc1.IfcProject;
+import org.bimserver.models.ifc2x3tc1.IfcProperty;
+import org.bimserver.models.ifc2x3tc1.IfcPropertySet;
+import org.bimserver.models.ifc2x3tc1.IfcPropertySetDefinition;
+import org.bimserver.models.ifc2x3tc1.IfcPropertySingleValue;
+import org.bimserver.models.ifc2x3tc1.IfcQuantityVolume;
+import org.bimserver.models.ifc2x3tc1.IfcReal;
+import org.bimserver.models.ifc2x3tc1.IfcRelContainedInSpatialStructure;
+import org.bimserver.models.ifc2x3tc1.IfcRelDecomposes;
+import org.bimserver.models.ifc2x3tc1.IfcRelDefines;
+import org.bimserver.models.ifc2x3tc1.IfcRelDefinesByProperties;
+import org.bimserver.models.ifc2x3tc1.IfcSpace;
+import org.bimserver.models.ifc2x3tc1.IfcSpatialStructureElement;
+import org.bimserver.models.ifc2x3tc1.IfcText;
+import org.bimserver.models.ifc2x3tc1.IfcValue;
+import org.bimserver.models.ifc2x3tc1.IfcVolumeMeasure;
+import org.bimserver.models.ifc2x3tc1.Tristate;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+
+public class IfcUtils {
+	public static Path getShortestAllPaths(IdEObject start, IdEObject end) {
+		Set<Path> paths = new LinkedHashSet<Path>();
+		Path basePath = new Path(start);
+		getAllPaths(basePath, end, paths, new HashSet<EObject>());
+		System.out.println("Number of paths: " + paths.size());
+		List<Path> pathList = new ArrayList<Path>(paths);
+		java.util.Collections.sort(pathList, new Comparator<Path>() {
+			@Override
+			public int compare(Path o1, Path o2) {
+				return o1.getLength() - o2.getLength();
+			}
+		});
+		return pathList.get(0);
+	}
+	
+	public static int getAllPaths(Path basePath, IdEObject otherObject, Set<Path> paths, Set<EObject> noConnectionPaths) {
+		int connectionsFound = 0;
+		if (noConnectionPaths.contains(basePath.getLastObject())) {
+			return 0;
+		}
+		for (EReference eReference : basePath.getLastObject().eClass().getEAllReferences()) {
+			Object reference = basePath.getLastObject().eGet(eReference);
+			if (reference != null) {
+				if (eReference.isMany()) {
+					@SuppressWarnings("rawtypes")
+					List list = (List)reference;
+					for (int index=0; index<list.size(); index++) {
+						Object refObject = list.get(index);
+						IdEObject refIdEObject = (IdEObject)refObject;
+						if (basePath.contains(refIdEObject)) {
+							// do nothing
+						} else if (refIdEObject == otherObject) {
+							paths.add(new Path(basePath, eReference, index, refIdEObject));
+							System.out.println(paths.size());
+						} else {
+							connectionsFound += getAllPaths(new Path(basePath, eReference, index, (IdEObject) refObject), otherObject, paths, noConnectionPaths);
+						}
+					}
+				} else {
+					if (basePath.contains((IdEObject) reference)) {
+						// do nothing
+					} else if (reference == otherObject) {
+						paths.add(new Path(basePath, eReference, -1, (IdEObject) reference));
+						System.out.println(paths.size());
+					} else {
+						connectionsFound += getAllPaths(new Path(basePath, eReference, -1, (IdEObject) reference), otherObject, paths, noConnectionPaths);
+					}
+				}
+			}
+		}
+		if (connectionsFound == 0) {
+			noConnectionPaths.add(basePath.getLastObject());
+		}
+		return connectionsFound;
+	}
+	
+	public static IfcBuildingStorey getIfcBuildingStorey(IfcProduct ifcProduct) {
+		if (ifcProduct instanceof IfcBuildingStorey) {
+			return (IfcBuildingStorey) ifcProduct;
+		}
+		for (IfcRelDecomposes ifcRelDecomposes : ifcProduct.getDecomposes()) {
+			IfcObjectDefinition relatingObject = ifcRelDecomposes.getRelatingObject();
+			if (relatingObject instanceof IfcBuildingStorey) {
+				return (IfcBuildingStorey)relatingObject;
+			}
+		}
+		if (ifcProduct instanceof IfcElement) {
+			IfcElement ifcElement = (IfcElement)ifcProduct;
+			for (IfcRelContainedInSpatialStructure ifcRelContainedInSpatialStructure : ifcElement.getContainedInStructure()) {
+				IfcSpatialStructureElement relatingStructure = ifcRelContainedInSpatialStructure.getRelatingStructure();
+				if (relatingStructure instanceof IfcBuildingStorey) {
+					return (IfcBuildingStorey) relatingStructure;
+				} else {
+					if (relatingStructure instanceof IfcSpace) {
+						return getIfcBuildingStorey(relatingStructure);
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public static List<IfcProduct> getChildren(IfcBuildingStorey ifcBuildingStorey) {
+		List<IfcProduct> list = new ArrayList<>();
+		for (IfcRelDecomposes ifcRelDecomposes : ifcBuildingStorey.getIsDecomposedBy()) {
+			for (IfcObjectDefinition ifcObjectDefinition : ifcRelDecomposes.getRelatedObjects()) {
+				if (ifcObjectDefinition instanceof IfcProduct) {
+					list.add((IfcProduct) ifcObjectDefinition);
+				}
+			}
+		}
+		return list;
+	}
+
+	public static IfcProject getIfcProject(IfcProduct ifcProduct) {
+		if (ifcProduct instanceof IfcProject) {
+			return (IfcProject) ifcProduct;
+		}
+		for (IfcRelDecomposes ifcRelDecomposes : ifcProduct.getDecomposes()) {
+			IfcObjectDefinition relatingObject = ifcRelDecomposes.getRelatingObject();
+			if (relatingObject instanceof IfcProject) {
+				return (IfcProject)relatingObject;
+			} else if (relatingObject instanceof IfcProduct){
+				return getIfcProject((IfcProduct) relatingObject);
+			}
+		}
+		if (ifcProduct instanceof IfcElement) {
+			IfcElement ifcElement = (IfcElement)ifcProduct;
+			for (IfcRelContainedInSpatialStructure ifcRelContainedInSpatialStructure : ifcElement.getContainedInStructure()) {
+				IfcSpatialStructureElement relatingStructure = ifcRelContainedInSpatialStructure.getRelatingStructure();
+				if (relatingStructure instanceof IfcProject) {
+					return (IfcProject) relatingStructure;
+				} else if (relatingStructure instanceof IfcBuildingStorey) {
+					return getIfcProject(relatingStructure);
+				} else {
+					if (relatingStructure instanceof IfcSpace) {
+						return getIfcProject(relatingStructure);
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	public static Tristate getBooleanProperty(IfcObject ifcObject, String propertyName) {
+		for (IfcRelDefines ifcRelDefines : ifcObject.getIsDefinedBy()) {
+			if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
+				IfcRelDefinesByProperties ifcRelDefinesByProperties = (IfcRelDefinesByProperties)ifcRelDefines;
+				IfcPropertySetDefinition propertySetDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
+				if (propertySetDefinition instanceof IfcPropertySet) {
+					IfcPropertySet ifcPropertySet = (IfcPropertySet)propertySetDefinition;
+					for (IfcProperty ifcProperty : ifcPropertySet.getHasProperties()) {
+						if (ifcProperty instanceof IfcPropertySingleValue) {
+							IfcPropertySingleValue propertyValue = (IfcPropertySingleValue)ifcProperty;
+							if (ifcProperty.getName().equals(propertyName)) {
+								IfcBoolean label = (IfcBoolean)propertyValue.getNominalValue();
+								return label.getWrappedValue();
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public static String getStringProperty(IfcObject ifcObject, String propertyName) {
+		for (IfcRelDefines ifcRelDefines : ifcObject.getIsDefinedBy()) {
+			if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
+				IfcRelDefinesByProperties ifcRelDefinesByProperties = (IfcRelDefinesByProperties)ifcRelDefines;
+				IfcPropertySetDefinition propertySetDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
+				if (propertySetDefinition instanceof IfcPropertySet) {
+					IfcPropertySet ifcPropertySet = (IfcPropertySet)propertySetDefinition;
+					for (IfcProperty ifcProperty : ifcPropertySet.getHasProperties()) {
+						if (ifcProperty instanceof IfcPropertySingleValue) {
+							IfcPropertySingleValue propertyValue = (IfcPropertySingleValue)ifcProperty;
+							if (ifcProperty.getName().equals(propertyName)) {
+								IfcValue nominalValue = propertyValue.getNominalValue();
+								if (nominalValue instanceof IfcLabel) {
+									return ((IfcLabel)nominalValue).getWrappedValue();
+								} else if (nominalValue instanceof IfcIdentifier) {
+									return ((IfcIdentifier)nominalValue).getWrappedValue();
+								} else if (nominalValue instanceof IfcBoolean) {
+									return ((IfcBoolean)nominalValue).getWrappedValue().toString();
+								} else if (nominalValue instanceof IfcText) {
+									return ((IfcText)nominalValue).getWrappedValue();
+								} else if (nominalValue instanceof IfcLengthMeasure) {
+									return String.valueOf(((IfcLengthMeasure)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcPlaneAngleMeasure) {
+									return String.valueOf(((IfcPlaneAngleMeasure)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcAreaMeasure) {
+									return String.valueOf(((IfcAreaMeasure)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcVolumeMeasure) {
+									return String.valueOf(((IfcVolumeMeasure)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcReal) {
+									return String.valueOf(((IfcReal)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcPowerMeasure) {
+									return String.valueOf(((IfcPowerMeasure)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcInteger) {
+									return String.valueOf(((IfcInteger)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcElectricCurrentMeasure) {
+									return String.valueOf(((IfcElectricCurrentMeasure)nominalValue).getWrappedValue());
+								} else if (nominalValue instanceof IfcLogical) {
+									return String.valueOf(((IfcLogical)nominalValue).getWrappedValue());
+								} else {
+									throw new RuntimeException(nominalValue.eClass().getName());
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public static List<String> listPropertyNames(IfcProduct ifcProduct) {
+		List<String> list = new ArrayList<>();
+		for (IfcRelDefines ifcRelDefines : ifcProduct.getIsDefinedBy()) {
+			if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
+				IfcRelDefinesByProperties ifcRelDefinesByProperties = (IfcRelDefinesByProperties)ifcRelDefines;
+				IfcPropertySetDefinition propertySetDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
+				if (propertySetDefinition instanceof IfcPropertySet) {
+					IfcPropertySet ifcPropertySet = (IfcPropertySet)propertySetDefinition;
+					for (IfcProperty ifcProperty : ifcPropertySet.getHasProperties()) {
+						list.add(ifcProperty.getName());
+					}
+				}
+			}
+		}
+		return list;
+	}
+
+	public static Double getIfcQuantityVolume(IfcProduct ifcProduct, String name) {
+		for (IfcRelDefines ifcRelDefines : ifcProduct.getIsDefinedBy()) {
+			if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
+				IfcRelDefinesByProperties ifcRelDefinesByProperties = (IfcRelDefinesByProperties)ifcRelDefines;
+				IfcPropertySetDefinition propertySetDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
+				if (propertySetDefinition instanceof IfcElementQuantity) {
+					if (propertySetDefinition.getName().equals("BaseQuantities")) {
+						IfcElementQuantity ifcElementQuantity = (IfcElementQuantity)propertySetDefinition;
+						for (IfcPhysicalQuantity ifcPhysicalQuantity : ifcElementQuantity.getQuantities()) {
+							if (ifcPhysicalQuantity instanceof IfcQuantityVolume) {
+								return ((IfcQuantityVolume)ifcPhysicalQuantity).getVolumeValue();
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	public static List<String> listElementQuantities(IfcProduct ifcProduct) {
+		List<String> list = new ArrayList<>();
+		for (IfcRelDefines ifcRelDefines : ifcProduct.getIsDefinedBy()) {
+			if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
+				IfcRelDefinesByProperties ifcRelDefinesByProperties = (IfcRelDefinesByProperties)ifcRelDefines;
+				IfcPropertySetDefinition propertySetDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
+				if (propertySetDefinition instanceof IfcElementQuantity) {
+					IfcElementQuantity ifcElementQuantity = (IfcElementQuantity)propertySetDefinition;
+					list.add(ifcElementQuantity.getName());
+				}
+			}
+		}
+		return list;
+	}
+
+	public static double[] getAbsolutePosition(IfcProduct ifcProduct) throws PlacementNotImplementedException {
+		return getAbsolutePosition(ifcProduct.getObjectPlacement());
+	}
+	
+	/**
+	 * Not finished, does not take into account the directions
+	 * 
+	 * @param ifcObjectPlacement
+	 * @return
+	 * @throws PlacementNotImplementedException
+	 */
+	public static double[] getAbsolutePosition(IfcObjectPlacement ifcObjectPlacement) throws PlacementNotImplementedException {
+		if (ifcObjectPlacement instanceof IfcGridPlacement) {
+			throw new PlacementNotImplementedException("IfcGridPlacement has not been implemented");
+		} else if (ifcObjectPlacement instanceof IfcLocalPlacement) {
+			IfcLocalPlacement ifcLocalPlacement = (IfcLocalPlacement)ifcObjectPlacement;
+			IfcAxis2Placement relativePlacement = ifcLocalPlacement.getRelativePlacement();
+			if (relativePlacement instanceof IfcAxis2Placement2D) {
+				throw new PlacementNotImplementedException("IfcAxis2Placement2D has not been implemented");
+			} else if (relativePlacement instanceof IfcAxis2Placement3D) {
+				IfcAxis2Placement3D ifcAxis2Placement3D = (IfcAxis2Placement3D)relativePlacement;
+				IfcObjectPlacement placementRelativeTo = ifcLocalPlacement.getPlacementRelTo();
+				if (placementRelativeTo == null) {
+					IfcCartesianPoint ifcCartesianPoint = ifcAxis2Placement3D.getLocation();
+					return new double[]{ifcCartesianPoint.getCoordinates().get(0), ifcCartesianPoint.getCoordinates().get(1), ifcCartesianPoint.getCoordinates().get(2)};
+				} else {
+					double[] relative = getAbsolutePosition(placementRelativeTo);
+					IfcCartesianPoint ifcCartesianPoint = ifcAxis2Placement3D.getLocation();
+					return new double[]{relative[0] + ifcCartesianPoint.getCoordinates().get(0), relative[1] + ifcCartesianPoint.getCoordinates().get(1), relative[2] + ifcCartesianPoint.getCoordinates().get(2)};
+				}
+			}
+		}
+		return new double[]{0d, 0d, 0d};
+	}
+}
